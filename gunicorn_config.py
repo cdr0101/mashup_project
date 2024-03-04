605411907dcefb2c90c17e6817c4d64b7b0fe4da
cdr0101 <117757108+cdr0101@users.noreply.github.com>
2024-03-04 07:49:21 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,1 +1,10 @@
-timeout = 600
+# gunicorn_config.py
+
+bind = '0.0.0.0:80'  # Bind to all network interfaces on port 80
+workers = 2  # Use 2 worker processes
+timeout = 600  # Set timeout to 60 seconds
+keepalive = 5  # Set keepalive timeout to 5 seconds
+accesslog = '-'  # Log access to stdout
+errorlog = '-'  # Log errors to stdout
+loglevel = 'info'  # Set log level to info
+worker_class = 'gthread'  # Use the gthread worker class
